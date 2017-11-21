310f43f1979fc81530f49d8c505820d187232769
georgecode <georgeczurlanis@gmail.com>
2017-11-20 17:20:51 -0800

    Add datePicker component place holder

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React, { Component } from 'react';
 // import YearDropDown from './components/yearDropDown/index.js';
 import PastPrice from './components/fetchPrice/index.js';
 import DateInput from './components/dateInput/index.js';
+import DatePicker from './components/datePicker/index.js';
+
+
 
 class App extends Component {
   render() {
@@ -11,6 +14,7 @@ class App extends Component {
       <div className="App">
         <PastPrice />
         <DateInput />
+        <DatePicker />
       </div>
     );
   }
---
+++ src/components/datePicker/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DatePicker extends Component{
+	render(){
+		return(
+			<h1>DatePicker </h1>
+			)
+	}
+}
+
+export default DatePicker
