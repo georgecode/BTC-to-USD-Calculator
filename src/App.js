402354031216846e20bca320ee6528360a509357
georgecode <georgeczurlanis@gmail.com>
2017-11-20 17:14:22 -0800

    Add working year picker

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 // import './App.css';
 // import YearDropDown from './components/yearDropDown/index.js';
 import PastPrice from './components/fetchPrice/index.js';
+import DateInput from './components/dateInput/index.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <PastPrice />
+        <DateInput />
       </div>
     );
   }
