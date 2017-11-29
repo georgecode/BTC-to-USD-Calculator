2013971c2eb2f66a81d82f87cc9b310b08c3fb34
georgecode <georgeczurlanis@gmail.com>
2017-11-29 03:20:09 -0800

    Add rocking bitcoins images

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
+import btcCoin from './images/btcCoin.svg';
 import CalendarPicker from './components/datePicker/index.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <img src={btcCoin} className="btcCoin small" alt="bitcoin" />
         <CalendarPicker />
+        <img src={btcCoin} className="btcCoin med" alt="bitcoin" />
+        <img src={btcCoin} className="btcCoin large" alt="bitcoin" />
       </div>
     );
   }
 }
 
 export default App;
+
+
+// import logo from './logo.svg';
+
+//<img src={logo} className="App-logo" alt="logo" />
